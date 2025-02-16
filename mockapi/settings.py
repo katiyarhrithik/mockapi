6496f72f628ca22de556a4fd99ed3ed14ba390bd
Hrithik Katiyar <104518431+katiyarhrithik@users.noreply.github.com>
2025-02-16 20:55:21 +0530

    Setting DEBUG to False

--- mockapi/settings.py
+++ mockapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7gs40i(+n^kqc^xpx7o%9m9km9mg3h+tnpl-mq=@5@nz8ducqj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
